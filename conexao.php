d5832cc67c47f84982d33df90a1898cdfccad3cd
ngrdbrnd <42627656+ngrdbrnd@users.noreply.github.com>
2018-08-23 08:29:03 -0300

    Update conexao.php

--- conexao.php
+++ conexao.php
@@ -1,11 +1,11 @@
 <?php
-  $servidor="localhost";//Endereço do servidor
-  $usuario="root";//Usuario de acesso ao servidor
-  $senha="";//$senha
-  $banco="cadastro";//Banco de dados que deseja acessar
+  $servidor="localhost";
+  $usuario="root";
+  $senha="";
+  $banco="cadastro";
   $conexao= new mysqli($servidor,$usuario,$senha,$banco);
-  //A linha cima cria a conexão com o banco de dados
-  if(mysqli_connect_error()){ // Verifica se a conexão gerou algum erro.
+ 
+  if(mysqli_connect_error()){ 
     echo"Erro ao conectar com MySQL;".mysqli_connect_error();
   }
 
